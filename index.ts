c90780d741a57fd6437eb371d119fb57892914b8
pgiannou <137771187+pgiannou@users.noreply.github.com>
2023-07-10 16:05:35 +0200

    3 files modified

--- index.ts
+++ index.ts
@@ -1,62 +1,74 @@
-// Import stylesheets
 import './style.scss';
 import * as echarts from 'echarts';
 
-// var myChart = echarts.init(
-//   document.querySelectorAll<HTMLElement>(
-//     '.main-dashboard-title-city-charts-item-content'
-//   )[0]
-// );
+var navigateFromCityCardToGraphButtons = document.querySelectorAll(
+  '.main-locations-city-cards-div-chevron-right'
+);
 
-var date = new Date();
+//for (let i = 0; i < navigateFromCityCardToGraphButtons.length; i++) {
+//navigateFromCityCardToGraphButtons[i].addEventListener('click', () => {
+getWeatherInfoAndMakeTemperatureGraph(1);
+//})
 
-date.setDate(date.getDate() - 1);
+function getWeatherInfoAndMakeTemperatureGraph(index: number) {
+  var selectedCity: string;
+  if (index == 0) {
+    selectedCity = 'Thessaloniki';
+  } else if (index == 1) {
+    selectedCity = 'Athens';
+  }
 
-let url =
-  'https://api.weatherapi.com/v1/history.json?key=c2c6271001c643d6a4390320231007&q=Thessaloniki&dt=' +
-  date.toLocaleDateString('en-GB').split('/').reverse().join('-');
+  var date = new Date();
 
-fetch(url, { method: 'GET' })
-  .then((result) => result.json())
-  .then((response) => {
-    console.log(response);
+  date.setDate(date.getDate() - 1);
 
-    var chartDom = document.querySelectorAll<HTMLElement>(
-      '.main-dashboard-title-city-charts-item-content'
-    )[0];
-    var myChart = echarts.init(chartDom);
+  let url =
+    'https://api.weatherapi.com/v1/history.json?key=c2c6271001c643d6a4390320231007&q=' +
+    selectedCity +
+    '&dt=' +
+    date.toLocaleDateString('en-GB').split('/').reverse().join('-');
 
-    // Calculating hours to put them in xAxis
-    var xAxisValues: string[] = [];
-    response['forecast']['forecastday']['0']['hour'].forEach((hour) => {
-      var hourTemp: any = Object.values(hour)['1'];
-      var specificHour = hourTemp.split(' ')[1];
-      xAxisValues.push(specificHour);
-    });
+  fetch(url, { method: 'GET' })
+    .then((result) => result.json())
+    .then((response) => {
+      var chartDom = document.querySelectorAll<HTMLElement>(
+        '.main-dashboard-title-city-charts-item-content'
+      )[0];
+      var myChart = echarts.init(chartDom);
 
-    // Calculating hourly temperatoures to put them in yAxis
-    var yAxisValues: string[] = [];
-    response['forecast']['forecastday']['0']['hour'].forEach((hour) => {
-      yAxisValues.push(hour.temp_c);
-    });
+      // Calculating hours to put them in xAxis
+      var xAxisValues: string[] = [];
+      response['forecast']['forecastday']['0']['hour'].forEach((hour) => {
+        var hourTemp: any = Object.values(hour)['1'];
+        var specificHour = hourTemp.split(' ')[1];
+        xAxisValues.push(specificHour);
+      });
 
-    var option;
-    option = {
-      xAxis: {
-        type: 'category',
-        data: xAxisValues,
-      },
-      yAxis: {
-        type: 'value',
-      },
-      series: [
-        {
-          data: yAxisValues,
-          type: 'line',
+      // Calculating hourly temperatoures to put them in yAxis
+      var yAxisValues: string[] = [];
+      response['forecast']['forecastday']['0']['hour'].forEach((hour) => {
+        yAxisValues.push(hour.temp_c);
+      });
+
+      var option;
+      option = {
+        xAxis: {
+          type: 'category',
+          data: xAxisValues,
         },
-      ],
-    };
+        yAxis: {
+          type: 'value',
+        },
+        series: [
+          {
+            data: yAxisValues,
+            type: 'line',
+          },
+        ],
+      };
 
-    option && myChart.setOption(option);
-  })
-  .catch((error) => console.log(error));
+      option && myChart.setOption(option);
+    })
+    .catch((error) => console.log(error));
+}
+//}
