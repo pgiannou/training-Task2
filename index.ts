16e6323ce0748bdda25770cd475a170184235b43
pgiannou <137771187+pgiannou@users.noreply.github.com>
2023-07-10 16:32:45 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -4,71 +4,81 @@ import * as echarts from 'echarts';
 var navigateFromCityCardToGraphButtons = document.querySelectorAll(
   '.main-locations-city-cards-div-chevron-right'
 );
+var chartDom = document.querySelectorAll<HTMLElement>(
+  '.main-dashboard-title-city-charts-item-content'
+)[0];
+var myChart = echarts.init(chartDom);
+for (let i = 0; i < navigateFromCityCardToGraphButtons.length; i++) {
+  navigateFromCityCardToGraphButtons[i].addEventListener('click', () => {
+    getWeatherInfoAndMakeTemperatureGraph(i);
+  });
 
-//for (let i = 0; i < navigateFromCityCardToGraphButtons.length; i++) {
-//navigateFromCityCardToGraphButtons[i].addEventListener('click', () => {
-getWeatherInfoAndMakeTemperatureGraph(1);
-//})
+  function getWeatherInfoAndMakeTemperatureGraph(index: number) {
+    var selectedCity: string;
+    if (index == 0) {
+      selectedCity = 'Thessaloniki';
+    } else if (index == 1) {
+      selectedCity = 'Athens';
+    }
 
-function getWeatherInfoAndMakeTemperatureGraph(index: number) {
-  var selectedCity: string;
-  if (index == 0) {
-    selectedCity = 'Thessaloniki';
-  } else if (index == 1) {
-    selectedCity = 'Athens';
-  }
+    var date = new Date();
 
-  var date = new Date();
+    date.setDate(date.getDate() - 1);
 
-  date.setDate(date.getDate() - 1);
+    let url =
+      'https://api.weatherapi.com/v1/history.json?key=c2c6271001c643d6a4390320231007&q=' +
+      selectedCity +
+      '&dt=' +
+      date.toLocaleDateString('en-GB').split('/').reverse().join('-');
 
-  let url =
-    'https://api.weatherapi.com/v1/history.json?key=c2c6271001c643d6a4390320231007&q=' +
-    selectedCity +
-    '&dt=' +
-    date.toLocaleDateString('en-GB').split('/').reverse().join('-');
+    fetch(url, { method: 'GET' })
+      .then((result) => result.json())
+      .then((response) => {
+        console.log(response);
 
-  fetch(url, { method: 'GET' })
-    .then((result) => result.json())
-    .then((response) => {
-      var chartDom = document.querySelectorAll<HTMLElement>(
-        '.main-dashboard-title-city-charts-item-content'
-      )[0];
-      var myChart = echarts.init(chartDom);
+        var dashboardTitle = document.querySelectorAll(
+          '.main-dashboard-title-text'
+        )[0];
+        dashboardTitle.innerHTML =
+          response['location']['name'] +
+          ', ' +
+          response['location']['region'] +
+          ', ' +
+          response['location']['tz_id'];
 
-      // Calculating hours to put them in xAxis
-      var xAxisValues: string[] = [];
-      response['forecast']['forecastday']['0']['hour'].forEach((hour) => {
-        var hourTemp: any = Object.values(hour)['1'];
-        var specificHour = hourTemp.split(' ')[1];
-        xAxisValues.push(specificHour);
-      });
+        // Calculating hours to put them in xAxis
+        var xAxisValues: string[] = [];
+        response['forecast']['forecastday']['0']['hour'].forEach((hour) => {
+          var hourTemp: any = Object.values(hour)['1'];
+          var specificHour = hourTemp.split(' ')[1];
+          xAxisValues.push(specificHour);
+        });
 
-      // Calculating hourly temperatoures to put them in yAxis
-      var yAxisValues: string[] = [];
-      response['forecast']['forecastday']['0']['hour'].forEach((hour) => {
-        yAxisValues.push(hour.temp_c);
-      });
+        // Calculating hourly temperatoures to put them in yAxis
+        var yAxisValues: string[] = [];
+        response['forecast']['forecastday']['0']['hour'].forEach((hour) => {
+          yAxisValues.push(hour.temp_c);
+        });
 
-      var option;
-      option = {
-        xAxis: {
-          type: 'category',
-          data: xAxisValues,
-        },
-        yAxis: {
-          type: 'value',
-        },
-        series: [
-          {
-            data: yAxisValues,
-            type: 'line',
+        var option;
+        option = {
+          xAxis: {
+            type: 'category',
+            data: xAxisValues,
           },
-        ],
-      };
+          yAxis: {
+            type: 'value',
+          },
+          series: [
+            {
+              data: yAxisValues,
+              type: 'line',
+            },
+          ],
+        };
 
-      option && myChart.setOption(option);
-    })
-    .catch((error) => console.log(error));
+        option && myChart.setOption(option, true);
+      })
+      .catch((error) => console.log(error));
+  }
 }
-//}
